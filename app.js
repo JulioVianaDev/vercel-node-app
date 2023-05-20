5fbbad82ed6c27aec74cdba8f6b1fb52c4b181bd
JulioVianaDev <JulioVianaDev@gmail.com>
2023-05-20 11:05:26 -0300

    colocando a rota principal

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(cors())
 app.use(express.json())
 const port = process.env.PORT || 3000
-app.get('/lucas', async function(req, res){
+app.get('/', async function(req, res){
   const response = await fetch('https://api.sampleapis.com/cartoons/cartoons2D')
       const data = await response.json()
       res.send(data)
